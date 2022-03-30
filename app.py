4d43909e032a07f20c098c6faa467c1479b21c9c
Sami Sidoroff <sami.sidoroff@gmail.com>
2022-03-30 23:50:24 +0300

    setting up app

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1 :D"
+
+@app.route("/page2")
+def page2():
+    return "Ja tämä on sivu 2! :D"
