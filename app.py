679e6152ab7affd856b193295a2b83b35294861e
Sami Sidoroff <sami.sidoroff@gmail.com>
2022-04-03 23:22:13 +0300

    started first version

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = db.session.execute("SELECT name FROM recipes")
+    recipes = sql.fetchall()
+    return render_template("index.html", recipes=recipes)
 
-@app.route("/page1")
+@app.route("/newcocktail")
 def page1():
-    return "Tämä on sivu 1 :D"
+    result = db.session.execute("SELECT name FROM ingredients")
+    ingredients = result.fetchall()
+    return render_template("newcocktail.html", ingredients=ingredients)
 
-@app.route("/page2")
-def page2():
-    return "Ja tämä on sivu 2! :D"
+@app.route("/cocktail/<cocktailname>")
+def cocktail(cocktailname):
+    sql = "SELECT ingredients FROM recipes WHERE name = (:cocktailname)"
+    sqlex = db.session.execute(sql, {"cocktailname":cocktailname})
+    ingredients = sqlex.fetchone()
+    #ingredients = result.split(",")
+    return render_template("cocktail.html", cocktailname=cocktailname, ingredients=ingredients)
