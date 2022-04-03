01f43a6dd8525b61cd0996a649cada9fec0761a6
Sami Sidoroff <sami.sidoroff@gmail.com>
2022-04-04 00:04:54 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 
 CREATE TABLE ingredients (id SERIAL PRIMARY KEY, name TEXT);
 
-CREATE TABLE recipes (id SERIAL PRIMARY KEY, name TEXT UNIQUE);
+CREATE TABLE recipes (id SERIAL PRIMARY KEY, name TEXT UNIQUE, ingredients TEXT);
 
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users, sent_at TIMESTAMP);
 
