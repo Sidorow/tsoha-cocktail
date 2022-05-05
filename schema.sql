9aec09c33397214c8bf577004535252c7fd0ff3b
Sami Sidoroff <sami.sidoroff@gmail.com>
2022-05-05 21:25:46 +0300

    Updated sql schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, is_admin BOOLEAN);
 
-CREATE TABLE recipe_ingredient (id SERIAL PRIMARY KEY, ingredient_id INTEGER REFERENCES ingredients(id), recipe_id INTEGER REFERENCES recipes(id), amount INTEGER);
+CREATE TABLE recipe_ingredient (id SERIAL PRIMARY KEY, ingredient_id INTEGER REFERENCES ingredients(id) ON DELETE CASCADE, recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE, amount INTEGER);
 
 CREATE TABLE ingredients (id SERIAL PRIMARY KEY, name TEXT UNIQUE, is_alc BOOLEAN);
 
 CREATE TABLE recipes (id SERIAL PRIMARY KEY, name TEXT UNIQUE, description TEXT);
 
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users(id), recipe_id INTEGER REFERENCES recipes(id), rating INTEGER, sent_at TIMESTAMP);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users(id) ON DELETE CASCADE, recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE, rating INTEGER, sent_at TIMESTAMP);
 
